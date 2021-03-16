ccddaa70ebda7826454e601df4e7a3201d3858d2
Sam Nolan <samnolan555@gmail.com>
2021-03-16 14:56:33 +1100

    Improve switch.sh

--- switch.sh
+++ switch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+export NIX_PLUGINS=$(nix-store -r $(nix-instantiate -E 'with import <nixpkgs> {}; nix-plugins'))
 export NIXPKGS_ALLOW_UNFREE=1
-nixos-rebuild switch --show-trace --option extra-builtins-file /etc/nixos/extra-builtins.nix
+nixos-rebuild switch --show-trace --option plugin-files $NIX_PLUGINS/lib/nix/plugins/libnix-extra-builtins.so --option extra-builtins-file /etc/nixos/extra-builtins.nix
